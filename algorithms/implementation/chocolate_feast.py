893ea720255fc18e216e3f8ee98136feb84968c0
Jason Keung <jason.s.keung@gmail.com>
2015-12-30 15:43:27 -0500

    adding chocolate_feast.py

--- algorithms/implementation/chocolate_feast.py
+++ algorithms/implementation/chocolate_feast.py
@@ -7,17 +7,13 @@ t = int(raw_input().strip())
 for a0 in xrange(t):
     n,c,m = raw_input().strip().split(' ')
     n,c,m = [int(n),int(c),int(m)]
-    num_chocolates = 0
-    wrappers = 0
+    num_chocolates = n/c
+    wrappers = num_chocolates
     
-    while n/c>0 or wrappers/m > 0:
-        if n/c>0:
-            num_chocolates += n / c
-            n -= num_chocolates * c
-            wrappers += num_chocolates
-        if wrappers / m >= 1:
-            bonus_chocolates = wrappers / m
-            num_chocolates += bonus_chocolates
-            wrappers = wrappers % m + bonus_chocolates
-        
+    while wrappers >= m:
+        bonus_chocolates = wrappers / m
+        num_chocolates += bonus_chocolates
+        wrappers = wrappers % m + bonus_chocolates
+            
     print num_chocolates
+
